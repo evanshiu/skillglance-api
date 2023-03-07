784872a0eb571ff8cf9c416e82855b8889842ad3
evanshiu <33865086+evanshiu@users.noreply.github.com>
2023-03-06 23:10:15 -0600

    unknown invalid key error

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'https://skillglance.onrender.com',
     'https://skillglance.com',
     'https://www.skillglance.com',
-    // 'http://localhost:3000'
+    'http://localhost:3000' //remove when deploying
 ]
 
 module.exports = allowedOrigins
